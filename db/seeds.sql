758b97874a3915b57c562a0df2e446bbc32723ad
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-21 15:04:10 -0600

    starter files, mysql and db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers VALUES (1, 'Classic', false);
+INSERT INTO burgers VALUES (2, 'Bacon Cheeseburger', false);
+INSERT INTO burgers VALUES (3, 'Spicy Jalapeno', true);
+INSERT INTO burgers VALUES (4, 'Mushroom Swiss', true);
+INSERT INTO burgers VALUES (5, 'Ahi Tuna Burger', true);
+INSERT INTO burgers VALUES (6, 'Bison Burger', false);
