2de0d32861ae1d1a1948df553b64a914f6231630
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-21 15:40:25 -0600

    file structure complete

---
+++ config/orm.js
@@ -0,0 +1,43 @@
+var connection = require("../config/connection.js");
+
+function selectAll() {
+
+
+}
+
+
+function insertOne() {
+
+
+}
+
+
+function updateOne() {
+
+
+
+}
+
+var orm = {
+    all: function () {
+
+    }
+},
+
+    create: function() {
+
+    }
+},
+
+update: function() {
+
+}
+},
+delete: function() {
+
+}
+};
+
+
+
+module.exports = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+
+    })
+})
+
+
+module.exports = router;
