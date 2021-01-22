e3427e4a47723ad8408dfee6b52d7bf8fd0e6162
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-21 18:08:12 -0600

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,50 @@ var router = express.Router();
 
 router.get("/", function (req, res) {
     burger.all(function (data) {
+        var object = {
+            burgers: data
+        };
+        console.log(object);
+        res.render("index", object);
 
-    })
-})
+    });
+});
 
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("api/burgers/:id", function (req, res) {
+    var status = "id = " + req.params.id;
+    console.log("status", status);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, status, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    var status = "id = " + req.params.id;
+
+    burger.delete(status, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
