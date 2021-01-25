ebc37e2f343e53187ecd875bda8addd1ba7b7b9e
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-25 14:00:38 -0600

    added burger-block handlebar

--- models/burger.js
+++ models/burger.js
@@ -17,48 +17,12 @@ var burger = {
             cb(res);
         });
     },
-    delete: function (condition, cb) {
-        orm.delete("burgers", condition, function (res) {
-            cb(res);
-        });
-    }
+    // delete: function (condition, cb) {
+    //     orm.delete("burgers", condition, function (res) {
+    //         cb(res);
+    //     });
+    // }
 };
 
 module.exports = burger;
 
-$(".submit").on("click", function (event) {
-    var id = $(this).data("id");
-    var uneaten = $(this).data("uneaten");
-
-    var eatNewBurger = {
-        eaten: uneaten
-    };
-
-    $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: eatNewBurger
-    }).then(
-        function () {
-            console.log("Burger is ready to be devoured", newSleep);
-            location.reload();
-        }
-    );
-});
-
-$(".create-form").on("submit", function (event) {
-    event.preventDefault();
-
-    var newBurger = {
-        name: $("#burgText").val().trim(),
-    };
-
-    $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-    }).then(
-        function () {
-            console.log("New Burger added");
-            location.reload();
-        }
-    );
-});
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,54 @@
+$(function () {
+    $("submit").on("click", function (event) {
+        var id = $(this).data("id");
+        var uneaten = $(this).data("uneaten");
+
+        var eatNewBurger = {
+            eaten: uneaten
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: eatNewBurger
+        }).then(
+            function () {
+                console.log("Burger is ready to be devoured", uneaten);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burgText").val().trim(),
+            devoured: false
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("New Burger added.");
+                location.reload();
+            }
+        );
+    });
+
+    $(".devoured").on("click", function (event) {
+        var id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT"
+        }).then(
+            function () {
+                console.log("Devoured", id);
+                location.reload();
+            }
+        );
+    });
+
+
+});
