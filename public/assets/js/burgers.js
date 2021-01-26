ff712f84d0fa1a9026497df1dc416048709568b8
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-26 13:01:41 -0600

    added css styling

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,23 +1,4 @@
 $(function () {
-    // $("submit").on("click", function (event) {
-    //     var id = $(this).data("id");
-    //     var uneaten = $(this).data("uneaten");
-
-    //     var eatNewBurger = {
-    //         eaten: uneaten
-    //     };
-    //     console.log(eatenNewBurger)
-
-    //     $.ajax("/api/burgers/" + id, {
-    //         type: "PUT",
-    //         data: eatNewBurger
-    //     }).then(
-    //         function () {
-    //             console.log("Burger is ready to be devoured", uneaten);
-    //             location.reload();
-    //         }
-    //     );
-    // });
 
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
